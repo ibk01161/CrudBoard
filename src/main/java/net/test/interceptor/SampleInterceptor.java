85fded36676e0d6cfeb2cdebde4a9447b22f6b8c
ibk01161 <rkxmsdk@naver.com>
2020-08-27 19:30:28 +0900

    Interceptor의 request, reponse 활용

--- src/main/java/net/test/interceptor/SampleInterceptor.java
+++ src/main/java/net/test/interceptor/SampleInterceptor.java
@@ -1,14 +1,19 @@
 package net.test.interceptor;
 
+import java.lang.reflect.Method;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class SampleInterceptor extends HandlerInterceptorAdapter {
 	
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+	// 인터셉터 테스트
+	
+	/*public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 		ModelAndView modelAndView) throws Exception {
 
 		System.out.println("post Handle...............");
@@ -23,5 +28,36 @@ public class SampleInterceptor extends HandlerInterceptorAdapter {
 		return true;
 		
 	}
+*/
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+
+		System.out.println("pre handle..................");
+		
+		HandlerMethod method = (HandlerMethod) handler;
+		Method methodObj = method.getMethod();
+		
+		System.out.println("Bean : " + method.getBean());
+		System.out.println("Method : " + methodObj);
+		
+		return true;
+		
+	}
+	
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+		System.out.println("post Handle...............");
+		
+		Object result = modelAndView.getModel().get("result");
+		
+		if(result != null) {
+			request.getSession().setAttribute("result", result);
+			response.sendRedirect("/crud/doA");
+		}
+		
+		
+	}
+	
 
 }
